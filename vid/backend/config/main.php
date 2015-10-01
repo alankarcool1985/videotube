8090d02a940a1700f1abf60d7590fa8e5c14287e
Alankar <alankar.singh1985@gmail.com>
2015-10-01 23:37:15 +0530

    first commit

---
+++ vid/backend/config/main.php
@@ -0,0 +1,45 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        'view' => [
+            'theme' => [
+                'basePath' => '@backend/web/themes/adminlte',
+                'baseUrl' => '@backend/web/themes/adminlte',
+                    'pathMap' => [
+                        '@backend/views' => '@backend/web/themes/adminlte',
+                        ],
+            ],
+
+        ],
+    ],
+    'params' => $params,
+];
