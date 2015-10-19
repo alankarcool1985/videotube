0841226c865de7f801bb1b090de34ae2111f54a4
Preet raj <pg00359079@techmahindra.com>
2015-10-20 00:28:27 +0530

    api folder

---
+++ vid/api/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'VhF1iMwpRB6xS1KoMNn_iuWCFTfg1Ve3',
+        ],
+
+
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ vid/api/modules/v1/controllers/CountryController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+
+/**
+ * Country Controller API
+ *
+ * @author Budi Irawan <deerawan@gmail.com>
+ */
+class CountryController extends ActiveController
+{
+	/*public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error','dashboard'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['logout', 'index'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }*/
+    
+    public $modelClass = 'api\modules\v1\models\Country';    
+}
+
+
